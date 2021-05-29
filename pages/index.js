1001759dfb08bd8a70786854e9cf3d8062009c1f
EloyB <eloyboone@hotmail.com>
2021-05-29 12:14:01 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,75 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { ImQuotesLeft } from "react-icons/im";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      <div className="h-screen w-full">
+        <div className="h-1/3 bg-secondary flex flex-col items-center justify-center">
+          <div>
+            <img className="w-40 h-auto" src="/logo.png" alt="logo" />
+          </div>
+          <div className="text-center text-primary">
+            <p>(ella-e-la) - french</p>
+            <p>(n.) - creator of unique foodconcepts</p>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div
+          className="h-2/3 bg-blue-50"
+          style={{
+            backgroundImage: "url(/header.jpeg)",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        ></div>
+      </div>
+      <div className="py-20 px-5">
+        <div className="w-full bg-secondary bg-opacity-50 px-8 pt-8">
+          <div className="space-y-5">
+            <h1 className="text-primary text-3xl">Qui est Ella?</h1>
+            <p className="text-primary">
+              Piep! Ik ben Ella, 23 jaar en foodlover in hart en nieren. Door de week ga ik door het
+              leven als een rechtenstudent, in de weekends sta ik klaar om jou en je gezelschap een
+              onvergetelijke ervaring te bezorgen. Een unieke setting en lekker eten staan hierbij
+              centraal. Dankzij corona, vond ik de tijd om mijn passie in concepten te gieten.
+              Welkom bij Ella est là.
+            </p>
+          </div>
+          <img className="transform translate-y-10" src="/ella2.png" alt="" />
+        </div>
+      </div>
+      <div className="px-5 py-20 flex flex-col items-center">
+        <ImQuotesLeft className="w-14 h-14 text-primary text-opacity-20" />
+        <p className="relative text-xl text-primary text-center">
+          Laat je zintuigen verrassen en geniet van een totaalbeleving
+        </p>
+      </div>
+      <div className="py-20 px-5 space-y-5">
+        <div className="w-full bg-secondary bg-opacity-50 px-8 pb-8">
+          <img className="transform -translate-y-10" src="/concepts.jpeg" alt="" />
+          <div className="space-y-5">
+            <h1 className="text-primary text-3xl">Food concepten: what’s in the name? </h1>
+            <p className="text-primary">
+              Ella est là werkt verschillende formules uit waarbij ‘food’ steeds de hoofdnoot
+              vormt/hoofdrol speelt. Maar het is meer dan dat! Elk food concept staat voor een
+              totaalbeleving waarbij zoveel mogelijk van onze zintuigen worden geprikkeld: zien –
+              horen – ruiken – voelen – proeven, het komt allemaal aan bod.
+            </p>
+          </div>
+        </div>
+        <div
+          className="w-full h-60 bg-red-50"
+          style={{
+            backgroundImage: "url(/dinee.jpeg)",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="w-full h-60 bg-red-50">{/* Tapasboxen */}</div>
+        <div className="w-full h-60 bg-red-50">{/* Ontbijtboxen */}</div>
+      </div>
+      <div>{/* contact */}</div>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#C68356",
+        secondary: "#FBE7D3",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
